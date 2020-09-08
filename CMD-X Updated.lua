d219edb6ea5468e6cba910227ce294b7ec5d957f
Adevvy <70031382+Adevvy@users.noreply.github.com>
2020-09-08 11:25:15 -0400

    Update CMD-X Updated.lua

--- CMD-X Updated.lua
+++ CMD-X Updated.lua
@@ -1,7 +1,7 @@
 ---------------------------------------|
 if not game:FindFirstChild("PlayerGui") then repeat wait() until game:IsLoaded() and game:GetService("CoreGui") and game:GetService("Players").LocalPlayer and game:GetService("Players").LocalPlayer.PlayerGui end
 -- Constraints: -----------------------|
-local ver = "1.8.0"
+local ver = "1.8.1"
 local nextupd = "05/09/2020"
 local cordCode = "coming soon"
 local changeLog = {
